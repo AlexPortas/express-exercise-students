6b18efecff11f588b48850be571f5b05c5d4947f
Alex <alexalonsoportas@gmail.com>
2022-01-18 18:30:36 +0100

    server5 hecho

---
+++ server5.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const { palindrome } = require("./utils/palindrome");
+const app = express()
+const port = 3000
+
+app.get('/check', (req, res) => {
+  let msg = "NO es un palíndromo";
+  if(req.query.palabra){
+    if (palindrome(req.query.palabra)) {
+      msg = "Es un palíndromo";
+    }
+  }
+  
+  res.status(200).send(`<h1>${msg}</h1>`)
+})
+
+app.use((req, res) => {
+  res.status(404).send('<h1>No encontro nada...</h1>')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http//localhost:${port}`)
+})
