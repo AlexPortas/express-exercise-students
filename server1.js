747a5b4a749fcbba31e732d4274d04256d73552f
Alex <alexalonsoportas@gmail.com>
2022-01-18 17:24:14 +0100

    server1 hecho

--- server1.js
+++ server1.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('<h1>El servidor funciona correctamente</h1>')
+  res.status(200).send('<h1>El servidor funciona correctamente</h1><p>Aquí iría el contenido del mensaje</p>')
 })
 
 app.listen(port, () => {
